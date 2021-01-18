32dba14e1834094d8bb15f20275fd19fc901a084
Grav <75702884+Gravxd@users.noreply.github.com>
2021-01-18 15:11:06 +0000

    Create function.lua

---
+++ function.lua
@@ -0,0 +1,21 @@
+webhookURL = 'webhook url goes here' -- Follow the instructions in the readme file if you don't know what this is!
+name = 'name for the webhook'
+message = 'Your message here' 
+
+
+footer = 'https://github.com/Gravxd/FiveM-To-Discord'
+function sendToDisc(title, message, footer)
+	local embed = {}
+	embed = {
+		{
+			["color"] = 65280, -- GREEN = 65280 --- RED = 16711680
+			["title"] = "**".. title .."**",
+			["description"] = "" .. message ..  "",
+			["footer"] = {
+				["text"] = footer,
+			},
+		}
+	}
+	PerformHttpRequest(webhookURL, 
+	function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+end 
